ba6242c36e7ad81f7049b7cd61f99328bb20b05c
Keiran Price <keiran.price@stfc.ac.uk>
2024-04-24 14:14:19 +0100

    Add Osiris Transform

---
+++ fia_api/scripts/transforms/osiris_transform.py
@@ -0,0 +1,51 @@
+"""
+Module provides the OSIRISTransform class, an implementation of the Transform abstract base class for Osiris instrument
+scripts.
+"""
+import logging
+from typing import Iterable
+
+from fia_api.core.model import Reduction
+from fia_api.scripts.pre_script import PreScript
+from fia_api.scripts.transforms.transform import Transform
+
+logger = logging.getLogger(__name__)
+
+
+class OsirisTransform(Transform):
+    """
+    OsirisTransform applies modifications to MARI instrument scripts based on reduction input parameters in a Reduction
+    entity.
+    """
+
+    def apply(self, script: PreScript, reduction: Reduction) -> None:
+        logger.info("Beginning Osiris transform for reduction %s...", reduction.id)
+        lines = script.value.splitlines()
+        # MyPY does not believe ColumnElement[JSONB] is indexable, despite JSONB implementing the Indexable mixin
+        # If you get here in the future, try removing the type ignore and see if it passes with newer mypy
+        for index, line in enumerate(lines):
+            if line.startswith("input_runs"):
+                lines[index] = "input_runs = " + (
+                    str(reduction.reduction_inputs["runno"])
+                    if isinstance(reduction.reduction_inputs["runno"], Iterable)
+                    else f"[{reduction.reduction_inputs['runno']}]"
+                )
+                continue
+            if line.startswith("calibration_file_path"):
+                lines[index] = f"calibration_file_path = \"{reduction.reduction_inputs['calibration_file_path']}\""
+                continue
+            if line.startswith("cycle ="):
+                lines[index] = f"cycle = \"{reduction.reduction_inputs['cycle_string']}\""
+                continue
+            if line.startswith("reflection = "):
+                lines[index] = f"reflection = \"{reduction.reduction_inputs['analyser']}\""
+                continue
+            if line.startswith("spectroscopy_reduction ="):
+                lines[index] = f"spectroscopy_reduction = {reduction.reduction_inputs['mode'] == 'spectroscopy'}"
+                continue
+            if line.startswith("diffraction_reduction = "):
+                lines[index] = f"diffraction_reduction = {reduction.reduction_inputs['mode'] == 'diffraction'}"
+                continue
+
+        script.value = "\n".join(lines)
+        logger.info("Transform complete for reduction %s", reduction.id)
--- test/scripts/transforms/test_factory.py
+++ test/scripts/transforms/test_factory.py
@@ -6,38 +6,19 @@ import pytest
 
 from fia_api.scripts.transforms.factory import get_transform_for_instrument
 from fia_api.scripts.transforms.mari_transforms import MariTransform
+from fia_api.scripts.transforms.osiris_transform import OsirisTransform
 from fia_api.scripts.transforms.test_transforms import TestTransform
 from fia_api.scripts.transforms.tosca_transform import ToscaTransform
 from fia_api.scripts.transforms.transform import MissingTransformError
 
 
-def test_get_transform_for_run_mari():
-    """
-    Test mari transform returned for mari
-    :return: None
-    """
-    instrument = "mari"
-    transform = get_transform_for_instrument(instrument)
-    assert isinstance(transform, MariTransform)
-
-
-def test_get_transform_for_tosca():
-    """
-    Test Tosca transform returned for tosca
-    :return: None
-    """
-    transform = get_transform_for_instrument("tosca")
-    assert isinstance(transform, ToscaTransform)
-
-
-def test_get_transform_for_run_test():
-    """
-    Test that TestTransform returned for TestInstrument
-    :return: None
-    """
-    instrument = "test"
-    transform = get_transform_for_instrument(instrument)
-    assert isinstance(transform, TestTransform)
+@pytest.mark.parametrize(
+    "name,expected_transform",
+    [("mari", MariTransform), ("tosca", ToscaTransform), ("test", TestTransform), ("osiris", OsirisTransform)],
+)
+def test_transform_factory(name, expected_transform):
+    """Parametrised factory tests"""
+    assert isinstance(get_transform_for_instrument(name), expected_transform)
 
 
 def test_get_transform_for_run_unknown_instrument():
