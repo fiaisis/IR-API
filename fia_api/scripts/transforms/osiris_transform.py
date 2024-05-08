366c3fdaea0cc146b56768feec05c82dbdf21c46
keiranjprice101 <44777678+keiranjprice101@users.noreply.github.com>
2024-05-08 14:11:23 +0100

    Add Osiris Transform (#311)
    * Add Osiris Transform
    
    * black auto commit
    
    * pylint
    
    * black auto commit
    
    * Add ignores
    
    * black auto commit
    
    * Add return type
    
    * Movie ignore
    
    * black auto commit
    
    * Include codecov token
    
    * Move ignores
    
    * black auto commit
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ fia_api/scripts/transforms/osiris_transform.py
@@ -0,0 +1,58 @@
+"""
+Module provides the OSIRISTransform class, an implementation of the Transform abstract base class for Osiris instrument
+scripts.
+"""
+
+import logging
+from typing import Iterable
+
+from fia_api.core.model import Reduction
+from fia_api.scripts.pre_script import PreScript
+from fia_api.scripts.transforms.transform import Transform
+
+logger = logging.getLogger(__name__)
+
+
+class OsirisTransform(Transform):
+    """
+    OsirisTransform applies modifications to MARI instrument scripts based on reduction input parameters in a Reduction
+    entity.
+    """
+
+    def apply(self, script: PreScript, reduction: Reduction) -> None:
+        logger.info("Beginning Osiris transform for reduction %s...", reduction.id)
+        lines = script.value.splitlines()
+        # MyPY does not believe ColumnElement[JSONB] is indexable, despite JSONB implementing the Indexable mixin
+        # If you get here in the future, try removing the type ignore and see if it passes with newer mypy
+        for index, line in enumerate(lines):
+            if line.startswith("input_runs"):
+                lines[index] = "input_runs = " + (
+                    str(reduction.reduction_inputs["runno"])  # type:ignore
+                    if isinstance(reduction.reduction_inputs["runno"], Iterable)  # type:ignore
+                    else f"[{reduction.reduction_inputs['runno']}]"  # type:ignore
+                )
+                continue
+            if line.startswith("calibration_file_path"):
+                lines[index] = (
+                    f"calibration_file_path = \"{reduction.reduction_inputs['calibration_file_path']}\""
+                )  # type:ignore
+                continue
+            if line.startswith("cycle ="):
+                lines[index] = f"cycle = \"{reduction.reduction_inputs['cycle_string']}\""  # type:ignore
+                continue
+            if line.startswith("reflection = "):
+                lines[index] = f"reflection = \"{reduction.reduction_inputs['analyser']}\""  # type:ignore
+                continue
+            if line.startswith("spectroscopy_reduction ="):
+                lines[index] = (
+                    f"spectroscopy_reduction = {reduction.reduction_inputs['mode'] == 'spectroscopy'}"
+                )  # type:ignore
+                continue
+            if line.startswith("diffraction_reduction = "):
+                lines[index] = (
+                    f"diffraction_reduction = {reduction.reduction_inputs['mode'] == 'diffraction'}"
+                )  # type:ignore
+                continue
+
+        script.value = "\n".join(lines)
+        logger.info("Transform complete for reduction %s", reduction.id)
