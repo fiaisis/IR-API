a7b618f3d6cc7f3418dbc41485bbbaaf79acdb02
Keiran Price <keiran.price@stfc.ac.uk>
2024-06-19 14:06:43 +0100

    Add experiment fetching

---
+++ fia_api/core/auth/experiments.py
@@ -0,0 +1,25 @@
+"""
+Module containing user experiment fetching
+"""
+
+import os
+from http import HTTPStatus
+
+import requests
+
+AUTH_URL = os.environ.get("AUTH_API_URL", "http://localhost:8001")
+API_KEY = os.environ.get("AUTH_API_KEY", "shh")
+
+
+def get_experiments_for_user_number(user_number: int) -> list[int]:
+    """
+    Given a user number fetch and return the experiment (RB) numbers for that user
+    :param user_number: The user number to fetch for
+    :return: List of ints (experiment numbers)
+    """
+    response = requests.get(
+        f"{AUTH_URL}/experiments?user_number={user_number}", timeout=30, headers={"Authorization": f"Bearer {API_KEY}"}
+    )
+    if response.status_code == HTTPStatus.OK:
+        return response.json()
+    return []
