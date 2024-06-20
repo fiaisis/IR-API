b86eeee91d06eaa454d1c34ca0bc8d7f910557de
Keiran Price <keiran.price@stfc.ac.uk>
2024-06-20 07:58:54 +0100

    Include handler

--- fia_api/fia_api.py
+++ fia_api/fia_api.py
@@ -9,11 +9,13 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from fia_api.core.exceptions import (
+    AuthenticationError,
     MissingRecordError,
     MissingScriptError,
     UnsafePathError,
 )
 from fia_api.exception_handlers import (
+    authentication_error_handler,
     missing_record_handler,
     missing_script_handler,
     unsafe_path_handler,
@@ -47,3 +49,4 @@ app.include_router(ROUTER)
 app.add_exception_handler(MissingRecordError, missing_record_handler)
 app.add_exception_handler(MissingScriptError, missing_script_handler)
 app.add_exception_handler(UnsafePathError, unsafe_path_handler)
+app.add_exception_handler(AuthenticationError, authentication_error_handler)
