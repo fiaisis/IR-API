6ddd2f343f89ed7cf86db98aebad2315c6fdf22a
Keiran Price <keiran.price@stfc.ac.uk>
2024-06-25 10:49:11 +0100

    Add additional service test

--- test/core/services/test_reduction.py
+++ test/core/services/test_reduction.py
@@ -6,7 +6,7 @@ from unittest.mock import Mock, patch
 
 import pytest
 
-from fia_api.core.exceptions import MissingRecordError
+from fia_api.core.exceptions import AuthenticationError, MissingRecordError
 from fia_api.core.services.reduction import (
     count_reductions,
     count_reductions_by_instrument,
@@ -75,3 +75,30 @@ def test_count_reductions_by_instrument(mock_spec_class, mock_repo):
     spec = mock_spec_class.return_value
     count_reductions_by_instrument("TEST")
     mock_repo.count.assert_called_once_with(spec.by_instrument("TEST"))
+
+
+@patch("fia_api.core.services.reduction._REPO")
+@patch("fia_api.core.services.reduction.get_experiments_for_user_number")
+def test_get_reduction_by_id_for_user_no_experiments(mock_get_exp, mock_repo):
+    """Test get_reduction_by_id when no experiments are permitted"""
+    reduction = Mock()
+    reduction.runs = [Mock(), Mock()]
+    mock_repo.find_one.return_value = reduction
+    mock_get_exp.return_value = []
+
+    with pytest.raises(AuthenticationError):
+        get_reduction_by_id(1, user_number=1234)
+
+
+@patch("fia_api.core.services.reduction._REPO")
+@patch("fia_api.core.services.reduction.get_experiments_for_user_number")
+def test_get_reduction_by_id_for_user_with_experiments(mock_get_exp, mock_repo):
+    """Test get_reduction_by_id_"""
+    reduction = Mock()
+    run = Mock()
+    run.experiment_number = 1234
+    reduction.runs = [run, Mock()]
+    mock_repo.find_one.return_value = reduction
+    mock_get_exp.return_value = [1234]
+
+    assert get_reduction_by_id(1, 1234) == reduction
